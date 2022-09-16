80032aef2a7b871dedb02b4d8ffe0154d2f70af0
Kraft <cpkraft@ccanet.rockwellcollins.com>
2022-09-16 15:34:19 -0500

    Add initial class stubs based on rough LLD

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+   // TODO: make sure to pass dependencies
+   // Make sure classes follow SOLID design
+   // Single Responsiblitiy
+   // Open-Closed
+   // Liskov Substitution
+   // Interface Segregation
+   // Dependency Inversion
+   cout << "Test: program start" << endl;
+}
